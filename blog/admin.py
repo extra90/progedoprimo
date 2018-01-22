f01f6f63166a2e50d757b11c49eb5e08757678ec
Alessio Marino <alessio_zero@live.it>
2018-01-22 11:11:30 +0000

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from blog import models
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['title']}
+
+class PostAdmin(admin.ModelAdmin):
+    propopulated_fields = {'slug': ['title']}
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Post, PostAdmin)
